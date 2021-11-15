7466a62f3b6468fcc15b0bfb54ff42b58be08c84
xxhapecheng <xxhapecheng@tencent.com>
2021-11-15 11:31:48 +0800

    base create

--- practice_Activity_cp2/app/src/main/java/com/example/practice_activity_cp2/MainActivity.java
+++ practice_Activity_cp2/app/src/main/java/com/example/practice_activity_cp2/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.practice_activity_cp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.b1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"click button",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"you click add",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"you click remove",Toast.LENGTH_LONG).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
