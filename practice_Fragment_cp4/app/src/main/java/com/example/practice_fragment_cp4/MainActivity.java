76bf496412a14b0fa91ffd2bfa3a85a136315bdc
xxhapecheng <xxhapecheng@tencent.com>
2021-11-17 11:27:03 +0800

    fragment simple use

--- practice_Fragment_cp4/app/src/main/java/com/example/practice_fragment_cp4/MainActivity.java
+++ practice_Fragment_cp4/app/src/main/java/com/example/practice_fragment_cp4/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.practice_fragment_cp4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button =(Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new AnotherRightFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
