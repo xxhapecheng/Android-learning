64ebd7d0419badb7b9e66a853d04f6a2163df685
xxhapecheng <xxhapecheng@tencent.com>
2021-11-19 11:45:04 +0800

    读取联系人数据(E

--- practice_ContentProvider_cp7/app/src/main/java/com/example/practice_contentprovider_cp7/MainActivity.java
+++ practice_ContentProvider_cp7/app/src/main/java/com/example/practice_contentprovider_cp7/MainActivity.java
@@ -2,38 +2,84 @@ package com.example.practice_contentprovider_cp7;
 
 import android.Manifest;
 import android.Manifest.permission;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    List<String> contactsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button makeCall=(Button)findViewById(R.id.make_call);
+
+        //查询联系人
+        ListView contactView=(ListView) findViewById(R.id.contacts_view);
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,contactsList);
         makeCall.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ContextCompat.checkSelfPermission(MainActivity.this, permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{permission.CALL_PHONE},1);
+//                if(ContextCompat.checkSelfPermission(MainActivity.this, permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+//                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{permission.CALL_PHONE},1);
+//                }
+//                else{
+//                    call();
+//                }
+                if(ContextCompat.checkSelfPermission(MainActivity.this, permission.READ_CONTACTS)!=PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{permission.READ_CONTACTS},1);
+                }else{
+                    readContacts();
                 }
-                else{
-                    call();
-                }
+
+
             }
         });
+
+        Toast.makeText(this,"finish",Toast.LENGTH_LONG);
+
+    }
+
+    private void readContacts() {
+        Cursor cursor=null;
+        try {
+            cursor=getContentResolver().query(Phone.CONTENT_URI,null,null,null,null);
+            if(cursor!=null){
+                while (cursor.moveToNext()){
+                    String displayname=cursor.getString((cursor.getColumnIndex(Phone.DISPLAY_NAME)));
+                    @SuppressLint("Range") String number=cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
+                    contactsList.add(displayname+"\n"+number);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(cursor!=null){
+                cursor.close();
+            }
+        }
     }
 
     private void call(){
