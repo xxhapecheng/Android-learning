c3620792247086f2332ac62508352d5239ad047e
xxhapecheng <xxhapecheng@tencent.com>
2021-11-19 11:05:02 +0800

    运行时权限：呼叫权限

--- practice_ContentProvider_cp7/app/src/main/java/com/example/practice_contentprovider_cp7/MainActivity.java
+++ practice_ContentProvider_cp7/app/src/main/java/com/example/practice_contentprovider_cp7/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.practice_contentprovider_cp7;
 
+import android.Manifest;
+import android.Manifest.permission;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button makeCall=(Button)findViewById(R.id.make_call);
+        makeCall.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this, permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{permission.CALL_PHONE},1);
+                }
+                else{
+                    call();
+                }
+            }
+        });
+    }
+
+    private void call(){
+        try {
+            Intent intent=new Intent();
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call();
+                } else {
+                    Toast.makeText(this, "you denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 }
