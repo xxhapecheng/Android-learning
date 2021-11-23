c8592aba5fe9e6db855c2399139436d2b9ea0855
xxhapecheng <xxhapecheng@tencent.com>
2021-11-23 11:26:37 +0800

    okhttp3使用练习

--- practice_video_cp9/app/src/main/java/com/example/practice_video_cp9/MainActivity.java
+++ practice_video_cp9/app/src/main/java/com/example/practice_video_cp9/MainActivity.java
@@ -14,6 +14,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,10 +35,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(v.getId()==R.id.send_request){
-                    sendRequestWithHttpURLConnection();
+                    sendRequestWithOkConnection();
                 }
             }
         });
+    }
+
+    private void sendRequestWithOkConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client=new OkHttpClient();
+                    Request request = new Request.Builder()
+                        .url("http://www.baidu.com")
+                        .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData=response.body().string();
+                    showResponse(responseData);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private void sendRequestWithHttpURLConnection() {
