e8e95d55dbfd37b09bd563de036715dd75fbf1bc
xxhapecheng <xxhapecheng@tencent.com>
2021-11-23 17:17:07 +0800

    xml数据解析

--- practice_video_cp9/app/src/main/java/com/example/practice_video_cp9/MainActivity.java
+++ practice_video_cp9/app/src/main/java/com/example/practice_video_cp9/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.practice_video_cp9;
 
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
@@ -12,11 +13,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,17 +59,83 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 try {
                     OkHttpClient client=new OkHttpClient();
+                    final ProxySelector proxySelector = new ProxySelector() {
+                        @Override
+                        public java.util.List<Proxy> select(final URI uri) {
+                            final List<Proxy> proxyList = new ArrayList<Proxy>(1);
+
+                            // Host
+                            final String host = uri.getHost();
+
+//                            // Is an internal host
+//                            if (host.startsWith("127.0.0.1") || StringUtils.contains(nonProxyHostsValue, host)) {
+//
+//                            } else {
+//                                // Add proxy
+//                                proxyList.add(new Proxy(Type.HTTP,
+//                                        new InetSocketAddress(proxyHostNameValue, Integer.parseInt(proxyPortValue))));
+//                            }
+                            proxyList.add(Proxy.NO_PROXY);
+                            return proxyList;
+                        }
+
+                        @Override
+                        public void connectFailed(URI arg0, SocketAddress arg1, IOException arg2) {
+                            throw new UnsupportedOperationException("Not supported yet.");
+                        }
+                    };
+
                     Request request = new Request.Builder()
-                        .url("http://www.baidu.com")
+                        .url("http://10.64.122.72/get_data.xml")
                         .build();
                     Response response = client.newCall(request).execute();
                     String responseData=response.body().string();
-                    showResponse(responseData);
+                    parseXMLWithPull(responseData);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }).start();
+    }
+
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            XmlPullParserFactory factory=XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int evenType= xmlPullParser.getEventType();
+            String id="";
+            String name="";
+            String version="";
+            while(evenType!=XmlPullParser.END_DOCUMENT){
+                String nodeName=xmlPullParser.getName();
+                switch (evenType){
+                    case XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id=xmlPullParser.nextText();
+                        }else if("name".equals(nodeName)){
+                            name=xmlPullParser.nextText();
+                        }else if("version".equals(nodeName)){
+                            version=xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    case XmlPullParser.END_TAG:{
+                        if("app".equals(nodeName)){
+                            Log.d("MainActivity","id is "+id);
+                            Log.d("MainActivity","name is "+name);
+                            Log.d("MainActivity","version is "+version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                evenType=xmlPullParser.next();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private void sendRequestWithHttpURLConnection() {
