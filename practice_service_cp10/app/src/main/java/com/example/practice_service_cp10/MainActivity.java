b618089a8c89cfc3889ab496de59059fc2979a21
xxhapecheng <xxhapecheng@tencent.com>
2021-11-24 11:34:14 +0800

    create empty project

--- practice_service_cp10/app/src/main/java/com/example/practice_service_cp10/MainActivity.java
+++ practice_service_cp10/app/src/main/java/com/example/practice_service_cp10/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.practice_service_cp10;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    public static  final int update_text=1;
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection =new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder=(MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startService=(Button) findViewById(R.id.start_service);
+        Button stopService=(Button) findViewById(R.id.stop_service);
+        Button bindService=(Button)findViewById(R.id.bind_service);
+        Button unbindService=(Button)findViewById(R.id.unbind_service);
+
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent=new Intent(MainActivity.this,MyService.class);
+                startService(startIntent);
+            }
+        });
+
+        stopService.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent stopIntent=new Intent(MainActivity.this,MyService.class);
+                stopService(stopIntent);
+            }
+        });
+
+        bindService.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bindIntent = new Intent(MainActivity.this,MyService.class);
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);
+            }
+        });
+
+        unbindService.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unbindService(connection);
+            }
+        });
+
     }
 }
---
+++ practice_service_cp10/app/src/main/java/com/example/practice_service_cp10/MyService.java
@@ -0,0 +1,48 @@
+package com.example.practice_service_cp10;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    private DownloadBinder mBinder = new DownloadBinder();
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService","onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService","onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService","onDestroy executed");
+    }
+
+    class DownloadBinder extends Binder{
+        public void startDownload(){
+            Log.d("MyService","startDownload executed");
+        }
+        public int getProgress(){
+            Log.d("Myservice","getProgress executed");
+            return 0;
+        }
+    }
+}
