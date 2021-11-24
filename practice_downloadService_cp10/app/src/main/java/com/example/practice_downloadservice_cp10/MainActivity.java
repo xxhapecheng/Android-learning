b1d0d6104ef68b224b11a4cc4dfe7b72edde1e79
xxhapecheng <xxhapecheng@tencent.com>
2021-11-24 16:05:01 +0800

    downloadService practice empty project

---
+++ practice_downloadService_cp10/app/src/main/java/com/example/practice_downloadservice_cp10/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.practice_downloadservice_cp10;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
